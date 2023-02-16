9ab3a8f22a9cbe3f8c00b34bdb4c58586534894e
dip497 <dipendra497@gmail.com>
2023-02-16 10:59:40 +0530

    Initial commit

---
+++ src/test/java/com/serviceops/ecommerce/EcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serviceops.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
