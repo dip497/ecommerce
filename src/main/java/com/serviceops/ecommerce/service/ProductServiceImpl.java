8e5658f04ee81b15255ab3dd7f58f99958ab6315
adarsh <sengaradarsh101@gmail.com>
2023-02-27 15:30:07 +0530

    Optimized

--- src/main/java/com/serviceops/ecommerce/service/ProductServiceImpl.java
+++ src/main/java/com/serviceops/ecommerce/service/ProductServiceImpl.java
@@ -9,7 +9,10 @@ import com.serviceops.ecommerce.entities.Product;
 import com.serviceops.ecommerce.repository.CategoryRepository;
 import com.serviceops.ecommerce.repository.ProductRepository;
 
+import com.serviceops.ecommerce.utils.Helper;
 import jakarta.transaction.Transactional;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -25,11 +28,13 @@ public class ProductServiceImpl implements ProductService{
     @Autowired
     private CategoryRepository categoryRepository;
 
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
     @Override
     public boolean createProduct(ProductDto product) {
         Product product1 = new Product(product.getProductName(), product.getProductDesc(), product.getProductPrice(), categoryRepository.findById(product.getProductCategory().getCategoryId()).get());
         product1.setCreatedBy(product.getCreatedBy());
         productRepository.save(product1);
+        logger.info("Product Created");
         return true;
     }
 
@@ -41,6 +46,7 @@ public class ProductServiceImpl implements ProductService{
         product1.setProductPrice(product.getProductPrice());
         product1.setUpdatedBy(product.getUpdatedBy());
         productRepository.save(product1);
+        logger.info("Updated Product ->",product1);
         return product;
     }
 //    public void deleteByproductSubCategory(Long Id){
@@ -50,6 +56,7 @@ public class ProductServiceImpl implements ProductService{
     @Override
     public boolean removeProduct(Long Id) {
         productRepository.deleteById(Id);
+        logger.info("Deleted Product Id"+ Id);
         return true;
     }
 
@@ -57,9 +64,9 @@ public class ProductServiceImpl implements ProductService{
     public ProductDto findProductById(Long Id) {
 
         Product product = productRepository.findById(Id).get();
-        ProductDto productDto = this.ProductToDto(product);
+        ProductDto productDto = Helper.EntityToDto(product);
         productDto.setProductId(product.getProductId());
-
+        logger.info("Product Found ->{}",product);
         return productDto;
     }
 
@@ -67,30 +74,24 @@ public class ProductServiceImpl implements ProductService{
     public List<ProductDto> getAllProducts() {
 
         List<Product> products = productRepository.findAll();
-        return products.stream().map(products1 -> ProductToDto(products1)).collect(Collectors.toList());
+        logger.info("All Products List ->{}",products);
+        return products.stream().map(products1 -> Helper.EntityToDto(products1)).collect(Collectors.toList());
+
     }
 
     @Override
     public CategoryDto findProductCategory(Long Id) {
-        return convert(categoryRepository.findById(Id).get());
+        logger.info("Product SubCategory"+Id);
+        return Helper.EntityToDto(categoryRepository.findById(Id).get());
     }
 
     @Override
     public List<ProductDto> findByProductCategory(CategoryDto category) {
+        logger.info("Find List of Products Acc tp Category ->{}",category);
         return productRepository.findByProductCategory(categoryRepository.findById(category.getCategoryId()).get()).stream().map(
-                product -> ProductToDto(product)).collect(Collectors.toList());
+                product -> Helper.EntityToDto(product)).collect(Collectors.toList());
 
     }
 
-    private CategoryDto convert(Category category) {
-        return new CategoryDto(category.getCategoryId(), category.getCategoryName());
-    }
-    private ProductDto ProductToDto(Product product)
-    {
-        ProductDto productDto = new ProductDto(product.getProductId(), product.getProductName(), product.getProductDesc(), product.getProductPrice(), convert(product.getProductCategory()));
-        productDto.setCreatedBy(product.getCreatedBy());
-        productDto.setUpdatedBy(product.getUpdatedBy());
-        return productDto;
-    }
 
 }
--- src/main/java/com/serviceops/ecommerce/utils/Helper.java
+++ src/main/java/com/serviceops/ecommerce/utils/Helper.java
@@ -1,5 +1,9 @@
 package com.serviceops.ecommerce.utils;
 
+import com.serviceops.ecommerce.dto.Category.CategoryDto;
+import com.serviceops.ecommerce.dto.Product.ProductDto;
+import com.serviceops.ecommerce.entities.Category;
+import com.serviceops.ecommerce.entities.Product;
 import com.serviceops.ecommerce.entities.Role;
 
 public class Helper {
@@ -18,4 +22,18 @@ public class Helper {
             return Role.ADMIN;
         }
     }
+    public static CategoryDto EntityToDto(Category category)
+    {
+        CategoryDto categoryDto = new CategoryDto(category.getCategoryId(), category.getCategoryName());
+        categoryDto.setCreatedBy(category.getCreatedBy());
+        categoryDto.setUpdatedBy(category.getUpdatedBy());
+        return categoryDto;
+    }
+    public static ProductDto EntityToDto(Product product){
+        ProductDto productDto = new ProductDto(product.getProductId(), product.getProductName(), product.getProductDesc(), product.getProductPrice(), EntityToDto(product.getProductCategory()));
+        productDto.setCreatedBy(product.getCreatedBy());
+        productDto.setUpdatedBy(product.getUpdatedBy());
+        return productDto;
+    }
+
 }
