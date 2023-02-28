27db50b9f00ec3d0e70eb28fbfc7dff2302eae3c
dipendra sharma <dipendra497@gmail.com>
2023-02-28 14:48:42 +0530

    register html improved

--- src/main/java/com/serviceops/ecommerce/repository/UserRepository.java
+++ src/main/java/com/serviceops/ecommerce/repository/UserRepository.java
@@ -7,6 +7,4 @@ import org.springframework.data.jpa.repository.Query;
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUserEmail(String userEmail);
 
-    @Query(value = "update user set user_password= ?1 where user_id=?2",nativeQuery = true)
-    void updatePassword(String password,String userEmail);
 }
